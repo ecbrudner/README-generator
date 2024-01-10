e51a622aa666d230d1b0a97e3154c351409ebd79
ecbrudner <148579054+ecbrudner@users.noreply.github.com>
2024-01-09 18:03:43 -0600

    created function to generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -33,13 +33,46 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  if (license == 'MIT License'){
+    return 'This project is licensed under the MIT License.'
+  } else if (license == 'Apache License 2.0'){
+    return 'This project is licensed under the Apache License 2.0.'
+  } else if (license == 'GNU General Public License v3.0'){
+    return 'This project is licensed under the GNU General Public License v3.0.'
+  } else if (license == 'Boost Software License 1.0'){
+    return 'This project is licensed under the Boost Software License 1.0.'
+  } else if (license == 'None'){
+    return '';
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Description 
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [How to Contribute](#How-to-Contribute)
+  * [Tests](#Tests)
+  * [License](#License)
+  * [Questions](#Questions)
+  ## Installation 
+  ${data.installation}
+  ## Usage 
+  ${data.usage}
+  ## License 
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+  ## How to Contribute 
+  ${data.contribution}
+  ## Tests 
+  ${data.test}
+  ## Questions 
+  You can find my GitHub profiel at www.github.com/${data.github}
+   or reach me at ${data.email} with any additional questions.
 `;
 }
 
